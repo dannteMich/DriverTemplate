42a005f71c30bc9593b2ee9aa69526d5c9b27d96
Michael <dannte@gmail.com>
2021-01-11 17:12:36 +0200

    initial commit

---
+++ Sample/Sample/Source.cpp
@@ -0,0 +1,9 @@
+#include <ntddk.h> 
+
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+	return STATUS_SUCCESS;
+}
+

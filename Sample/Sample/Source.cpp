b45d0a928a9bcba5e3f017ea3d6b598095f6951d
Michael <dannte@gmail.com>
2021-01-11 17:19:01 +0200

    basics

--- Sample/Sample/Source.cpp
+++ Sample/Sample/Source.cpp
@@ -1,9 +1,17 @@
 #include <ntddk.h> 
 
+void DriverUnload(_In_ PDRIVER_OBJECT DriverObject);
+
+extern "C"
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
-	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
+	
+	DriverObject->DriverUnload = DriverUnload;
 	return STATUS_SUCCESS;
 }
 
+void DriverUnload(_In_ PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+}
